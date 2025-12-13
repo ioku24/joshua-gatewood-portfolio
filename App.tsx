ed3a08a56a82ceb90019eb3b1d6f85f1e67d18bb
Joshua Gatewood <josh@Mac.lan>
2025-12-13 00:55:26 -0500

    fix: Scroll to top when navigating between pages

--- App.tsx
+++ App.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import ScrollToTop from './components/ScrollToTop';
 import HomePage from './pages/HomePage';
 import PortfolioOverview from './pages/PortfolioOverview';
 import ProjectPage from './pages/ProjectPage';
@@ -7,6 +8,7 @@ import ProjectPage from './pages/ProjectPage';
 function App() {
   return (
     <BrowserRouter>
+      <ScrollToTop />
       <div className="bg-background min-h-screen text-slate-50 selection:bg-indigo-500 selection:text-white font-sans">
         <Routes>
           <Route path="/" element={<HomePage />} />
